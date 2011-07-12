037a8163df4cfe6302429d83f0eaab1fdbd67e9e
termie <github@anarkystic.com>
2011-07-12 11:54:22 -0700

    update enc to work with djeep

--- lib/enc.py
+++ lib/enc.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import json
-import os
 import re
 import sys
 import urllib2
@@ -14,13 +13,14 @@ config = {
     'kick_pass': '<admin pass>'
 }
 
+# TODO(termie): why does a json file have a yaml extension?
 with open('/etc/puppet/enc.yaml') as f:
     yaml_config = json.load(f)
     config.update(yaml_config)
 
 # got the config... fetch kick info from os-kick.
 
-hardware_uri = config['kick_uri'] + '/admin/hardware/view'
+hardware_uri = config['kick_uri'] + '/api/hardware/'
 
 auth_handler = urllib2.HTTPBasicAuthHandler()
 auth_handler.add_password(realm = 'kick dingus',
@@ -31,7 +31,7 @@ auth_handler.add_password(realm = 'kick dingus',
 opener = urllib2.build_opener(auth_handler)
 urllib2.install_opener(opener)
 
-req = urllib2.Request(hardware_uri, None, { 'Accept': 'application/json' })
+req = urllib2.Request(hardware_uri, None, {'Accept': 'application/json'})
 r = urllib2.urlopen(req)
 
 oskick_details = json.load(r)
@@ -42,41 +42,20 @@ oskick_details = json.load(r)
 
 hostname = sys.argv[1] #.split('.')[0]
 
-machine_info = [x for x in oskick_details['hardware'] if x['hostname'] == hostname ]
+machine_info = [x for x in oskick_details if x['hostname'] == hostname ]
 if not machine_info:
     print "Can't find machine info"
     sys.exit(1)
 
-cluster_id = machine_info[0]['cluster_id']
+cluster_id = machine_info['cluster']['id']
+cluster_name = machine_info['cluster']['short_name']
 
-# now, pull the short name of the cluster
 
-cluster_uri = config['kick_uri'] + '/admin/clusters/view/%s' % cluster_id
-req = urllib2.Request(cluster_uri, None, { 'Accept': 'application/json' })
+# TODO(termie): why does the json file have a yaml extension?
+cluster_description = '/etc/puppet/%s.yaml' % cluster_name
+with open(cluster_description) as fd:
+    cluster_details = json.load(fd)
 
-cluster_name = 'test'
-
-try:
-    r = urllib2.urlopen(req)
-    cluster_name = json.load(r)['short_name']
-except urllib2.URLError, e:
-    if e.code != 404:
-        sys.exit(1)
-
-# print oskick_details
-
-cluster_details = {}
-
-try:
-    cluster_description = '/etc/puppet/%s.yaml' % cluster_name
-    fd = open(cluster_description)
-except Exception as e:
-    print 'Error opening %s' % cluster_description
-    print e
-    sys.exit(1)
-
-cluster_details = json.load(fd)
-fd.close()
 
 # walk through the cluster config, find all matches for
 # each role.
@@ -85,32 +64,30 @@ roles_by_machine = {}
 machines_by_role = {}
 
 for role, rolematch in cluster_details['cluster'].items():
-    for hwinfo in oskick_details['hardware']:
+    for hwinfo in oskick_details:
         host = hwinfo['hostname']
 
         if host and re.match(rolematch, host):
-            if not roles_by_machine.has_key(host):
-                roles_by_machine[host] = []
-            roles_by_machine[host].append(role)
+            host_roles = roles_by_machine.get(host, set())
+            host_roles.add(role)
+            roles_by_machine[host] = host_roles
 
-            if not machines_by_role.has_key(role):
-                machines_by_role[role] = []
-
-            if host not in machines_by_role[role]:
-                machines_by_role[role].append(host)
+            role_hosts = machines_by_role.get(role, set())
+            role_hosts.add(host)
+            machines_by_role[role] = role_hosts
 
 # Now generate the roles list
 enc_manifest = { 'classes': [], 'parameters': {} }
 
 #print "looking for roles for %s" % hostname
 
-if not roles_by_machine.has_key(hostname):
+if hostname not in roles_by_machine:
     # not found..
     print "No roles!"
     sys.exit(1)
 
-for role in roles_by_machine[hostname]:
-    enc_manifest['classes'].append(role)
+enc_manifest['classes'] = list(roles_by_machine[hostname])
+
 
 for key, value in cluster_details['options'].items():
     new_value = value
@@ -118,8 +95,8 @@ for key, value in cluster_details['options'].items():
     matchgroup = re.match("#\{(.*)\}", str(value))
     if matchgroup:
         # return the ip of the node (or first match)
-        matching_host = [ x['ip_address'] for x in oskick_details['hardware']
-                          if re.match(matchgroup.group(1),x['hostname']) ]
+        matching_host = [x['ip_address'] for x in oskick_details
+                         if re.match(matchgroup.group(1), x['hostname'])]
         if matching_host:
             new_value = matching_host[0]
 
@@ -131,5 +108,3 @@ for key, value in cluster_details['options'].items():
     enc_manifest['parameters'][key] = new_value
 
 print json.dumps(enc_manifest, indent=2)
-
-
